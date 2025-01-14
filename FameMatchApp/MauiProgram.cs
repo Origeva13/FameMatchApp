35983667a89a2cc352fa0fc4896d13e921b07f81
User <User@LAB3-3>
2025-01-14 11:39:30 +0200

    עיצוב

--- FameMatchApp/MauiProgram.cs
+++ FameMatchApp/MauiProgram.cs
@@ -19,6 +19,7 @@ namespace FameMatchApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Broadway3D.ttf", "Broadway3D");
                 })
                 .RegisterDataServices()
                 .RegisterPages()
